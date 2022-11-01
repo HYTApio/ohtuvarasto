b06cf00ded97d520634c55fd187101227dab5ba3
HYTapio <tapio.uzal@helsinki.fi>
2022-11-01 18:50:48 +0200

    varasto testing 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,38 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_korjaa_uuden_varaston_tilavuuden(self):
+        self.assertAlmostEqual(Varasto(-10).tilavuus, 0.0)
+
+    def test_korjaa_uuden_varaston_saldon(self):
+        self.assertAlmostEqual(Varasto(10, -10).saldo, 0.0)
+    
+    def test_ylimääräinen_saldo_pois(self):
+        self.assertAlmostEqual(Varasto(10, 20).saldo, 10)
+
+    def test_lisays_ei_vahenna_saldoa(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.lisaa_varastoon(-1)
+        # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_lisays_ei_lisaa_enemman_tilavuus(self):
+        self.varasto.lisaa_varastoon(12)
+        # hukkaan menee 2
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_ota_ei_lisaa_saldoa(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.ota_varastosta(-2)
+        # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_ota_ei_ota_enemman_kuin_voi(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.ota_varastosta(10)
+        # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+    
+    def test_print(self):
+        self.assertEqual(print(self.varasto), None)
